72a7b180941337fd2770377f6ab83e72c786e03c
T <T>
2021-02-28 20:33:27 +0100

    Fix URL parsing

--- SmartLoad.lua
+++ SmartLoad.lua
@@ -2,22 +2,19 @@ function descriptor()
     return {
         title = "Smart Playlist Extender";
         description = "Extends the playlist by files in last item's directory";
-        version = "0.1.1";
-        author = "thebamby";
+        version = "0.2.1";
+        author = "thebamby/Tcc100";
         capabilities = {}
     }
 end
 
 function activate()
-    -- vlc.playlist.enqueue({{path = ""}})
-    -- if (true) then return end
-
     local playlistItems = vlc.playlist.get("normal", false).children
     -- vlc.msg.dbg(vlc.playlist.current())
-    if (#playlistItems == 0) then 
+    if (#playlistItems == 0) then
         vlc.msg.info("[SmartLoad] No items in playlist!")
         vlc.deactivate()
-        return 
+        return
     end
 
     local curItem = playlistItems[#playlistItems] -- playlist.current_item()
@@ -27,21 +24,20 @@ function activate()
     for key,item in ipairs(playlistItems) do
         table.insert(ignoredPaths, getFilename(item.path))
     end
-    
+
     if (not (string.sub(curItem.path, 1, 7) == "file://")) then
         vlc.msg.info("[SmartLoad] Last item is not a proper file!")
         vlc.deactivate()
-        return 
+        return
     end
 
     local folderPath = getFolder(curItem.path)
     local curItemName = getFilename(curItem.path)
     -- for k,v in pairs(vlc.net.opendir(folderPath)) do vlc.msg.dbg(tostring(k) .. " " .. tostring(v)) end
 
-    local files = vlc.io.readdir(folderPath)
+    -- local files = vlc.io.readdir(folderPath)
+    local files = vlc.net.opendir(folderPath)
     table.sort(files)
-
-    local beforeFile = true
 
     for _, item in ipairs(files) do
         if ((curItemName >= item) or (arrayContains(item, ignoredPaths))) then
@@ -65,32 +61,19 @@ function arrayContains(value, arr)
 end
 
 function getFolder(path)
-    local folderPath = string.match(path, ".*[\\\\/]")
-    folderPath = string.gsub(folderPath, "file://", "")
-    -- vlc.msg.dbg(folderPath)
-    return unpercent(folderPath)
+    local url = vlc.strings.url_parse(path)
+    local path = url.path
+    path = vlc.strings.decode_uri(path)
+    path = string.match(path, ".*[\\\\/]")
+    return path
 end
 
 function getFilename(path)
-    local filename = string.match(path, "[^\\\\/]*$")
-    return unpercent(filename)
-end
-
-function unpercent(str)
-
-    return vlc.strings.url_parse(str)
-    -- local matchCount
-    -- local parsedString
-    -- local percMatch = "%%%x%x"
-    -- function parsePercent(str)
-    --    local varCode = tonumber(string.sub(str, 2), 16)
-    --     -- vlc.msg.dbg("matched " .. str .. " charCode " .. varCode .. " char: " .. string.char(varCode))
-    --     return string.char(varCode) -- should be utf8.char(varCode)
-    -- end
-    
-    -- parsedString, matchCount = string.gsub(str, percMatch, parsePercent)
-    -- -- vlc.msg.dbg(matchCount .. " " .. parsedString)
-    -- return parsedString
+    local url = vlc.strings.url_parse(path)
+    local path = url.path
+    path = vlc.strings.decode_uri(path)
+    path = string.match(path, "[^\\\\/]*$")
+    return path
 end
 
 function deactivate()
